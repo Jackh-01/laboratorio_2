01797af05430ade192769b796878a27360f074e8
jackh-01 <jackh.narvaez@udea.edu.co>
2021-04-29 23:47:42 -0500

    problema 13
    problema 13 e incluí los requisitos de esta practica

--- laboratorio_2/main.cpp
+++ laboratorio_2/main.cpp
@@ -11,11 +11,18 @@ int main()
     switch (menu) {
         case 3:
             res= problema3(&c1[0], c2);
-            cout<<res<<endl;
+            cout<<res<<endl;        
         break;
 
 
+    case 13:{
+        int total, mat[6][8]={{0,3,4,0,0,0,6,8}, {5,13,6,0,0,0,2,3,}, {2,6,2,7,3,0,10,0}, {0,0,4,15,4,1,6,0}, {0,0,7,12,6,9,10,4}, {5,0,6,10,6,4,8,0,}};
+        total=problema13(&mat[0][0]);
+        cout<<"el total de estrella es: "<<total<<endl;
     }
+
+    }
+
 
     return 0;
 }
--- laboratorio_2/problemas.cpp
+++ laboratorio_2/problemas.cpp
@@ -19,3 +19,18 @@ int longitud(char *c){
     return i+1;
 
 }
+
+int problema13(int *mat)
+{
+    int estr=0,matriz[6][8];
+    float logica;
+    for (int f=0,i=0;f<6;f++)for(int c=0;c<8;c++,i++) matriz[f][c]=*(mat+i);
+    for(int f=1;f<5;f++){
+        for(int c=1;c<7;c++){
+            logica=(matriz[f][c]+matriz[f][c-1]+matriz[f][c+1]+matriz[f-1][c]+matriz[f+1][c])/float(5);
+            if (logica>6) estr++;
+
+        }
+    }
+    return estr;
+}
